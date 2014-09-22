ae80db0b55b8ae4f465a6c12b4e264165b907532
Mattias Wikström <mw222rs@student.lnu.se>
2014-09-22 10:12:04 +0200

    uppgraderat till nivå B

--- Godtycklig Lönerevision/Godtycklig Lönerevision/Program.cs
+++ Godtycklig Lönerevision/Godtycklig Lönerevision/Program.cs
@@ -10,103 +10,140 @@ namespace Godtycklig_Lönerevision
     {
         static void Main(string[] args)
         {
-            while (true)
+            bool loop = true;
+            while (loop == true)
             {
-                int numberOfSalaries = ReadInt("Ange antal löner att mata in: ");
+
+                int numberOfSalaries = 0;
                 while (numberOfSalaries < 2)
                 {
-                    Console.BackgroundColor = ConsoleColor.DarkRed;
-                    Console.WriteLine("\nDu måste mata in minst två löner för att kunna göra en beräkning!\n");
-                    Console.ResetColor();
                     numberOfSalaries = ReadInt("Ange antal löner att mata in: ");
+                    if (numberOfSalaries < 2)
+                    {
+                        ViewMessage("\nDu måste mata in minst två löner för att kunna göra en beräkning!\n", true);
+                    }
                 }
 
-                // Kalla på metoden ProcessSalaries.          
                 Console.WriteLine();
-                ProcessSalaries(numberOfSalaries);
+                int[] salaries = ReadSalaries(numberOfSalaries);
 
-                // Tryck på escape för att avsluta, valfri knapp startar om.
-                Console.BackgroundColor = ConsoleColor.DarkGreen;
-                Console.WriteLine("\nValfri tangent påbörjar ny beräkning - [ESC] avslutar programmet.\n");
-                Console.ResetColor();
-                if (Console.ReadKey(true).Key == ConsoleKey.Escape)
-                {
-                    return;
-                }
+                ViewResult(salaries);
+
+                loop = IsContinuing();
             }
         }
 
         static int ReadInt(string prompt)
-        {           
+        {
             while (true)
             {
                 Console.Write(prompt);
                 string temp = Console.ReadLine();
                 try
                 {
-                    return int.Parse(temp);                    
+                    if (int.Parse(temp) < 0)
+                    {
+                        ViewMessage("Inga negativa siffror, tack!", true);
+                    }
+                    else
+                    {
+                        return int.Parse(temp);
+                    }
                 }
                 catch (FormatException)
                 {
                     Console.BackgroundColor = ConsoleColor.DarkRed;
-                    Console.WriteLine("FEL! \"{0}\" kan inte tolkas som ett heltal!", temp);
+                    ViewMessage("FEL! \"" + temp + "\" kan inte tolkas som ett heltal!", true);
                     Console.ResetColor();
                 }
-            }           
+            }
         }
 
-        static void ProcessSalaries(int count)
+        static int[] ReadSalaries(int count)
         {
             // Skapa arrayen salaries och ge den värden.
             int[] salaries = new int[count];
             for (int i = 0; i < count; i++)
-			{			
-            salaries[i] = ReadInt("Ange lön nummer " + (i + 1) + ": ");
-            }
-
-            // Kopiera array salaries till salariesCopy för senare redovisning.
-            int[] salariesCopy = new int[count];
-            Array.Copy(salaries, salariesCopy, count);
-
-            // Sortera salaries.
-            Array.Sort(salaries);
-            Console.WriteLine();
-
-            // Lite variabler.
-            int medianSalary;
-            int meanSalary = (int)salaries.Average();
-            int salarySpread = salaries.Max() - salaries.Min();
-
-            // Räkna ut median.
-            if (count % 2 != 0)
             {
-                medianSalary = salaries[count / 2];
+                salaries[i] = ReadInt("Ange lön nummer " + (i + 1) + ": ");
             }
-            else
-            {
-                medianSalary = (salaries[(count - 1) / 2] + salaries[(count + 1) / 2]) / 2;
-            }
+            return salaries;
+        }
 
-            
+        static void ViewResult(int[] salaries)
+        {
+            int medianSalary = GetMedian(salaries);
+            int averageSalary = (int)salaries.Average();
+            int salaryDispersion = GetDispersion(salaries);
 
             Console.WriteLine("\n---------------------------\n");
             Console.WriteLine("Medianlön:     {0,5:c0}", medianSalary);
-            Console.WriteLine("Medellön:      {0,5:c0}", meanSalary);
-            Console.WriteLine("Lönespridning: {0,5:c0}", salarySpread);
+            Console.WriteLine("Medellön:      {0,5:c0}", averageSalary);
+            Console.WriteLine("Lönespridning: {0,5:c0}", salaryDispersion);
             Console.WriteLine("\n---------------------------\n");
 
             // Skriv ut alla löner, i inmatad ordning, tre på varje rad.
-            for (int i = 0; i < salariesCopy.Length; i++)
+            for (int i = 0; i < salaries.Length; i++)
+            {
+                Console.Write("{0,8}", salaries[i]);
+                if ((i % 3) == 2)
                 {
-                    Console.Write("{0,8}", salariesCopy[i]);
-                    if ((i % 3) == 2) 
-                    {
-                        Console.WriteLine();
-                    }
+                    Console.WriteLine();
                 }
+            }
             Console.WriteLine();
-        }    
+        }
 
+        static int GetDispersion(int[] source)
+        {
+            return source.Max() - source.Min();
+        }
 
+        static int GetMedian(int[] source)
+        {
+            int medianSalary;
+            int[] copy = new int[source.Length];
+            Array.Copy(source, copy, source.Length);
+            Array.Sort(copy);
+            if (copy.Length % 2 != 0)
+            {
+                return medianSalary = copy[copy.Length / 2];
+            }
+            else
+            {
+                return medianSalary = (copy[(copy.Length - 1) / 2] + copy[(copy.Length + 1) / 2]) / 2;
+            }
+
+        }
+
+        static bool IsContinuing()
+        {
+            ViewMessage("\nValfri tangent påbörjar ny beräkning - [ESC] avslutar programmet.\n", false);
+            if (Console.ReadKey(true).Key == ConsoleKey.Escape)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
+        }
+
+        static void ViewMessage(string message, bool isError)
+        {
+            if (isError == true)
+            {
+                Console.BackgroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine(message);
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.DarkBlue;
+                Console.WriteLine(message);
+                Console.ResetColor();
+            }
+        }
     }
 }
